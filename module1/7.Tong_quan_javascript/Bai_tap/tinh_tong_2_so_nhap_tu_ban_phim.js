3ca6ef55644b2c52c997f57d95184257c55b1241
nguyenluongphuc <nguyenluongphuc.dh@gmail.com>
2020-08-11 18:30:53 +0700

    update bai tap phan 7 ngay11thang8nam2020

---
+++ module1/7.Tong_quan_javascript/Bai_tap/tinh_tong_2_so_nhap_tu_ban_phim.js
@@ -0,0 +1,6 @@
+let soThu1 = prompt("Hãy nhập vào số thứ nhất");
+let soThu2 = prompt("Hãy nhập vào số thứ hai");
+let so1 = parseInt(soThu1);
+let so2 = parseInt(soThu2);
+let tongHaiSo = so1 + so2;
+confirm("Tổng hai số bạn vừa nhập là :" + tongHaiSo);
